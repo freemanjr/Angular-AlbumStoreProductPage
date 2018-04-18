eabd27341d5001bd5490a0a327ff294d20e11e6c
Jonathan Freeman <freemanj6@gmail.com>
2018-04-18 15:51:04 -0500

    part3: added product service constructor parameter.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,8 +8,10 @@ export class ProductService {
   private _http: Http;
   private _albumUrl: string = '../assets/album.json';
 
-  constructor() { }
-  getAlbum(id: Number) {
+  constructor(http: Http) { 
+    this._http = http;
+  }
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
